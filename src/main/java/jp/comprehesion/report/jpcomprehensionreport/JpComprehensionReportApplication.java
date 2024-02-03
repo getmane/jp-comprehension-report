0d4f4e9d89814fe30c26c8f566b327f5890932f0
mrhul <mr.huliganer@gmail.com>
2024-02-03 16:33:30 +0100

    Project setup

---
+++ src/main/java/jp/comprehesion/report/jpcomprehensionreport/JpComprehensionReportApplication.java
@@ -0,0 +1,13 @@
+package jp.comprehesion.report.jpcomprehensionreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpComprehensionReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpComprehensionReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/comprehesion/report/jpcomprehensionreport/JpComprehensionReportApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.comprehesion.report.jpcomprehensionreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpComprehensionReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
